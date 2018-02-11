efce4b5c1cb5018f16247c248aa9efd8ff96866a
Peter Roberts <dad@Peters-Mac-mini.local>
2018-02-11 00:00:59 -0600

    initial commit

---
+++ ConduitXF/View.fs
@@ -0,0 +1,13 @@
+﻿namespace ConduitXF
+
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+
+open Gjallarhorn.Bindable
+
+open Conduit
+
+type MainPage() as this = 
+    inherit Xamarin.Forms.ContentPage()
+    do
+        this.LoadFromXaml(typeof<MainPage>) |> ignore
